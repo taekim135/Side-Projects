0e0d5219d3c2c71cddce1c158243e3b72a436dc9
taekim135 <tkim363@uwo.ca>
2025-09-25 11:22:05 +0900

    minor update

--- Password Generator/PWGen/resources/generator.js
+++ Password Generator/PWGen/resources/generator.js
@@ -16,9 +16,11 @@ const numbers = ['0','1','2','3','4','5','6','7','8','9']
 const specials = ['@','#','$','&','%','!','?']
 
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const formReceived = document.querySelector("form");
-    
+
     // listen for a submit event, then run processForm function
     formReceived.addEventListener("submit", processForm);
 
