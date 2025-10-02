7bbf0318c43d8e73f29ebf283d910d30543f40fc
taekim135 <tkim363@uwo.ca>
2025-10-02 11:39:49 +0900

    minor changes

--- DiscordBot/bot_main.py
+++ DiscordBot/bot_main.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 # bot itself but in Discord's API world, bot = user/client talking to server
 class myClient(discord.Client):
 
-    bot = commands.Bot(command_prefix='!')
+    bot = commands.Bot(command_prefix='$')
 
     
     # async = run/execute simultaenously w/o stopping main thread
@@ -22,17 +22,32 @@ class myClient(discord.Client):
         print("Logged in as {0}!".format(self.user))
 
 
-    # tells discord.py that it's a command
-    # decorator pytho function into a command
-    @bot.command(name='hello')
-    async def greet(ctx):
-        await ctx.send(f'Hi {ctx.author.name}!')
+
+    # decorator python function into a command (functionName = user types)
+    # @command = other users can type pre-defined calls using symbol ($hello)
+    # @event = built-in calls you can overwrite - on_member_join (when an event happens)
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send(f'Hi {ctx.author.mention}!')
+
+
+    @bot.command()
+    async def vote(ctx,*,question):
+        # embedded message
+        poll = discord.Embed(title = "Vote", description = question)
+        pollMessage = await ctx.send(embed = poll)
+
+        await pollMessage.add_reaction("Good")
+        await pollMessage.add_reaction("Bad")
+        
 
     
     # function to retrieve a quote
-    def get_quote():
+    @bot.command()
+    def get_quote(ctx):
         res = requests.get("https://zenquotes.io/api/random")
         data = json.loads(res.text)
+        print(data)
 
 
 # specifies the bot's purpose
