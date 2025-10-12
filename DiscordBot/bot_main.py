156d11b67b0de100760fe0dd77fc1a2b3886b8b5
taekim135 <tkim363@uwo.ca>
2025-10-13 00:19:32 +0900

    Minor Changes

--- DiscordBot/bot_main.py
+++ DiscordBot/bot_main.py
@@ -8,46 +8,7 @@ import requests
 import json
 from dotenv import load_dotenv
 
-
-# interacts with Discord API
-# extended from discord.Client
 # bot itself but in Discord's API world, bot = user/client talking to server
-class myClient(discord.Client):
-
-    bot = commands.Bot(command_prefix='$')
-
-    
-    # async = run/execute simultaenously w/o stopping main thread
-    async def on_ready(self):
-        print("Logged in as {0}!".format(self.user))
-
-
-
-    # decorator python function into a command (functionName = user types)
-    # @command = other users can type pre-defined calls using symbol ($hello)
-    # @event = built-in calls you can overwrite - on_member_join (when an event happens)
-    @bot.command()
-    async def hello(ctx):
-        await ctx.send(f'Hi {ctx.author.mention}!')
-
-
-    @bot.command()
-    async def vote(ctx,*,question):
-        # embedded message
-        poll = discord.Embed(title = "Vote", description = question)
-        pollMessage = await ctx.send(embed = poll)
-
-        await pollMessage.add_reaction("Good")
-        await pollMessage.add_reaction("Bad")
-        
-
-    
-    # function to retrieve a quote
-    @bot.command()
-    def get_quote(ctx):
-        res = requests.get("https://zenquotes.io/api/random")
-        data = json.loads(res.text)
-        print(data)
 
 
 # specifies the bot's purpose
@@ -57,10 +18,43 @@ intents.message_content = True
 
 # client/bot activated using token
 load_dotenv()
+bot = commands.Bot(command_prefix = "$", intents=intents)
 
 
-client = myClient(command_prefix = "$", intents=intents)
-client.run(os.getenv("API_KEY"))
+# async = run/execute simultaenously w/o stopping main thread
+async def on_ready(self):
+    print("Logged in as {0}!".format(self.user))
+
+
+
+# decorator python function into a command (functionName = user types)
+# @command = other users can type pre-defined calls using symbol ($hello)
+# @event = built-in calls you can overwrite - on_member_join (when an event happens)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi {ctx.author.mention}!')
+
+
+@bot.command()
+async def vote(ctx,*,question):
+    # embedded message
+    poll = discord.Embed(title = "Vote", description = question)
+    pollMessage = await ctx.send(embed = poll)
+
+    await pollMessage.add_reaction("Good")
+    await pollMessage.add_reaction("Bad")
+    
+
+
+# function to retrieve a quote
+@bot.command()
+async def quote(ctx):
+    res = requests.get("https://zenquotes.io/api/random")
+    data = json.loads(res.text)
+    await print(data)
+
+
+bot.run(os.getenv("API_KEY"))
 
 
 
