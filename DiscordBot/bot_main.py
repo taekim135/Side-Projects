8e07c9caed78c51aa13f0f8444b960e01aefb0e5
taekim135 <tkim363@uwo.ca>
2025-10-13 14:10:56 +0900

    Role Assign, debug logs

--- DiscordBot/bot_main.py
+++ DiscordBot/bot_main.py
@@ -4,6 +4,7 @@
 import discord
 from discord.ext import commands
 import os
+import logging
 import requests
 import json
 from dotenv import load_dotenv
@@ -15,6 +16,8 @@ from dotenv import load_dotenv
 # default from discrod.dev config (messaging for now)
 intents = discord.Intents.default()
 intents.message_content = True  
+handler = logging.FileHandler(filename = 'bot.log', encoding = "utf-8", mode = 'w')
+roles = "Visitor"
 
 # client/bot activated using token
 load_dotenv()
@@ -29,10 +32,16 @@ async def on_ready(self):
 
 # decorator python function into a command (functionName = user types)
 # @command = other users can type pre-defined calls using symbol ($hello)
+#               talking directly to the bot
 # @event = built-in calls you can overwrite - on_member_join (when an event happens)
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hi {ctx.author.mention}!')
+
+
+async def on_member_join(person):
+    await person.send(f"Welcome to the Server {person.name}! You can prompt me using the $ symbol")
+
 
 
 @bot.command()
@@ -54,7 +63,31 @@ async def quote(ctx):
     await print(data)
 
 
-bot.run(os.getenv("API_KEY"))
+
+# assign role to user
+# name parameter = name of role defined in the server
+# the role needs to exist first in the server.
+@bot.command()
+async def assing(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=roles)
+
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} Your new title: {roles}")
+    else:
+        await ctx.send("Role does not exist")
+    
+
+
+
+
+
+
+
+
+
+
+bot.run(os.getenv("API_KEY"), log_handler=handler, log_level=logging.DEBUG)
 
 
 
